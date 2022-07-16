9534c08c9c226d674f56d7f1f31ab39dea798cf1
Retr0A <alexxretr0a@gmail.com>
2022-07-16 18:58:18 +0300

    Initial Commit

---
+++ Creativengine/src/Test.cpp
@@ -0,0 +1,17 @@
+#include "Test.h"
+
+#include <iostream>
+
+namespace Creativengine {
+
+	void Run()
+	{
+		std::cout << "Hello World!" << std::endl;
+	}
+
+}
+
+int main()
+{
+	Creativengine::Run();
+}
---
+++ Game/src/GameApp.cpp
@@ -0,0 +1,8 @@
+namespace Creativengine {
+	__declspec(dllimport) void Run();
+}
+
+int main()
+{
+	Creativengine::Run();
+}
